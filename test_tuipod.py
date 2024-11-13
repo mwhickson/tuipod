3391de22cb6f6af3547a1f746cf2b512bb74ae8f
Matthew Hickson <matthewhickson@outlook.com>
2024-11-12 22:01:09 -0500

    fleshed out models and added instantiation smoke tests

--- test_tuipod.py
+++ test_tuipod.py
@@ -6,13 +6,75 @@
 
 import unittest
 
-import tuipod
+from tuipod import Podcast, Episode, Player, Search, PodcastApp
 
+
+TEST_PODCAST_NAME = "A Podcast"
+TEST_EPISODE_NAME = "An Episode"
+TEST_PODCAST_FEED_URL = "https://localhost/podcast"
+TEST_EPISODE_FEED_URL = "https://localhost/podcast/episode1.mp3"
+TEST_PODCAST_DESCRIPTION = "A test podcast."
+TEST_EPISODE_DESCRIPTION = "A test podcast episode."
+TEST_EPISODE_DURATION_MINUTES = 5
 
 class TestSmokeTest(unittest.TestCase):
 
     def test_sanity_check(self):
         self.assertEqual(1, 1)
+
+
+class TestModelTests(unittest.TestCase):
+
+    def test_create_podcast(self):
+        p = Podcast(TEST_PODCAST_NAME, TEST_PODCAST_FEED_URL, TEST_PODCAST_DESCRIPTION)
+        
+        self.assertIsNotNone(p)
+        self.assertEqual(p.title, TEST_PODCAST_NAME)
+        self.assertEqual(p.url, TEST_PODCAST_FEED_URL)
+        self.assertEqual(p.description, TEST_PODCAST_DESCRIPTION)
+        self.assertIsNotNone(p.episodes)
+        self.assertEqual(p.episodes, [])
+
+
+    def test_create_episode(self):
+        e = Episode(TEST_EPISODE_NAME, TEST_EPISODE_FEED_URL, TEST_EPISODE_DESCRIPTION, TEST_EPISODE_DURATION_MINUTES)
+        
+        self.assertIsNotNone(e)
+        self.assertEqual(e.title, TEST_EPISODE_NAME)
+        self.assertEqual(e.url, TEST_EPISODE_FEED_URL)
+        self.assertEqual(e.description, TEST_EPISODE_DESCRIPTION)
+        self.assertEqual(e.duration, TEST_EPISODE_DURATION_MINUTES)
+
+
+    def test_add_episode_to_podcast(self):
+        p = Podcast(TEST_PODCAST_NAME, TEST_PODCAST_FEED_URL, TEST_PODCAST_DESCRIPTION)
+        e = Episode(TEST_EPISODE_NAME, TEST_EPISODE_FEED_URL, TEST_EPISODE_DESCRIPTION, TEST_EPISODE_DURATION_MINUTES)
+        
+        p.add_episode(e)
+
+        self.assertIsNotNone(p.episodes)
+        self.assertEqual(len(p.episodes), 1)
+
+        self.assertIsNotNone(e)
+        self.assertEqual(p.episodes[0].title, TEST_EPISODE_NAME)
+        self.assertEqual(p.episodes[0].url, TEST_EPISODE_FEED_URL)
+        self.assertEqual(p.episodes[0].description, TEST_EPISODE_DESCRIPTION)
+        self.assertEqual(p.episodes[0].duration, TEST_EPISODE_DURATION_MINUTES)
+
+
+    def test_remove_episode_to_podcast(self):
+        p = Podcast(TEST_PODCAST_NAME, TEST_PODCAST_FEED_URL, TEST_PODCAST_DESCRIPTION)
+        e = Episode(TEST_EPISODE_NAME, TEST_EPISODE_FEED_URL, TEST_EPISODE_DESCRIPTION, TEST_EPISODE_DURATION_MINUTES)
+                
+        p.add_episode(e)
+
+        self.assertIsNotNone(p.episodes)
+        self.assertEqual(len(p.episodes), 1)
+
+        p.remove_episode(TEST_EPISODE_FEED_URL)
+
+        self.assertIsNotNone(p.episodes)
+        self.assertEqual(p.episodes, [])
 
 
 if __name__ == "__main__":
--- tuipod.py
+++ tuipod.py
@@ -4,23 +4,66 @@
 # 2024-11-12: Matthew Hickson
 #
 
-
-class Podcast:
-    pass
-
+APPLICATION_NAME = "tuipod"
+APPLICATION_VERSION = "2024-11-12.5c24b1e90d6c4ae28faceec6bbcdff7a"
 
 class Episode:
-    pass
+
+    def __init__(self, title: str, url: str, description: str, duration: int) -> None:
+        self.title = title
+        self.url = url
+        self.description = description
+        self.duration = duration
+
+
+class Podcast:
+
+    def __init__(self, title: str, url: str, description: str) -> None:
+        self.title = title
+        self.url = url
+        self.description = description
+        self.episodes = []
+
+
+    def add_episode(self, episode: Episode) -> None:
+        self.episodes.append(episode)
+
+
+    def remove_episode(self, url: str) -> None:
+        for e in self.episodes:
+            if e.url == url:
+                self.episodes.remove(e)
+                break
 
 
 class Player:
-    pass
+
+    def __init__(self, episode: Episode, position_seconds: int) -> None:
+        self.episode = episode
+        self.position_seconds = position_seconds
 
 
 class Search:
-    pass
+
+    def __init__(self, number_results: int, sort_order: str) -> None:
+        self.number_results = number_results
+        self.sort_order = sort_order
+
+
+class PodcastApp:
+
+    def __init__(self) -> None:
+        self.name = APPLICATION_NAME
+        self.version = APPLICATION_VERSION
+        self.podcasts = []
+
+
+    def run(self) -> None:
+        print("%s (version %s)" % (self.name, self.version))
 
 
 if __name__ == "__main__":
-    pass
+
+    app = PodcastApp()
+    app.run()
 
